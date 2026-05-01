a6e2f4079b09c63248e10b2f5bc0413fc34665c8
joelsewhere <joelsewhere@gmail.com>
2026-05-01 00:55:08 -0500

    typo

--- sql/orders.sql
+++ sql/orders.sql
@@ -15,4 +15,4 @@ INNER JOIN item_sums
 WHERE orders.ingested_date = '{{ ti.xcom_pull(task_ids="metadata")["ingested_date"] }}'
 AND orders.declared_total >= 0 -- drop negative order totals
 -- drop mismatching order totals
-AND ABS(order.declared_total - item_sums.computed_total) / NULLIF(item_sums.computed_total, 0) <= 0.30
+AND ABS(orders.declared_total - item_sums.computed_total) / NULLIF(item_sums.computed_total, 0) <= 0.30
